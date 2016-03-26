6983cdd47b12bd0b121c5e02d01a8c2adc3121b5
Edwin Goddard <edwin.goddard@gmail.com>
2016-03-26 12:37:54 +0000

    createing and styling header

--- header.php
+++ header.php
@@ -26,6 +26,7 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+		<a href="/"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.min.svg" class="logo" alt="Cliona Joyce Logo" /></a>
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
