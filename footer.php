a6fe53807d1a85c452898bfbff8a143e5ad35a69
Edwin Goddard <edwin.goddard@gmail.com>
2016-04-26 21:50:06 +0100

    customising footer

--- footer.php
+++ footer.php
@@ -13,11 +13,19 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'clionajoyce' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'clionajoyce' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'clionajoyce' ), 'clionajoyce', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+	<footer id="colophon" class="site-footer footer" role="contentinfo">
+		<div class="site-info g-2up">
+      <div class="gi">
+        <p class="copyright">&copy; <?php date("Y") ?> Cliona Joyce. All rights reserved.</p>
+        </div>
+        <div class="contact-details gi">
+        <p><a class="phone" href="tel:087 342 8486">087 342 8486</a></p>
+        <p><a class="email" href="mailto:cliona@clionajoyce.ie">cliona@clionajoyce.ie</a></p>
+        <p>Corrib Medical Centre,<br>17/18 Mill Street,<br>Galway</p>
+      </div>
+			 <!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'clionajoyce' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'clionajoyce' ), 'WordPress' ); ?></a> -->
+			 <!-- <span class="sep"> | </span> -->
+			 <?php //printf( esc_html__( 'Theme: %1$s by %2$s.', 'clionajoyce' ), 'clionajoyce', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
